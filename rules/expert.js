6ee1b06857add098d7d01c3fad4a6e0727677c37
Christophe Hurpeau <christophe@hurpeau.com>
2018-06-28 15:02:05 +0200

    refactor: remove import/no-named-as-default

--- rules/expert.js
+++ rules/expert.js
@@ -5,10 +5,6 @@ module.exports = {
     // http://eslint.org/docs/rules/no-plusplus
     'no-plusplus': 'off',
 
-    // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-named-as-default.md
-    // `export default from './foo'` should work
-    'import/no-named-as-default': 'off',
-
     // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/prefer-default-export.md
     'import/prefer-default-export': 'off',
   },
