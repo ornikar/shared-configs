559c46995d9eea73ee6e2829e60e35cc2af9467e
Christophe Hurpeau <christophe.hurpeau+github@gmail.com>
2018-07-10 09:32:28 +0000

    feat: reenable rules curly and quotes disabled by prettier config (#3)
    BREAKING CHANGE: rules `curly` and `quotes` reenabled

--- plugins/prettier.js
+++ plugins/prettier.js
@@ -6,5 +6,13 @@ module.exports = {
 
   rules: {
     'prettier/prettier': 'error',
+
+    // https://github.com/prettier/eslint-config-prettier#curly
+    // prettier doesn't enforce {} with multiline
+    curly: ['error', 'multi-line'],
+
+    // https://github.com/prettier/eslint-config-prettier#quotes
+    // prettier doesn't change backtick to single
+    quotes: ['error', 'single', { avoidEscape: true }],
   },
 };
