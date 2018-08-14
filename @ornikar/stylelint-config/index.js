c94ca84a3787b7c18a6a7c1a9f4000c2f33d6cca
Christophe Hurpeau <christophe@hurpeau.com>
2018-08-14 11:29:56 +0200

    feat: add rules

--- @ornikar/stylelint-config/index.js
+++ @ornikar/stylelint-config/index.js
@@ -2,4 +2,29 @@
 
 module.exports = {
   extends: ['stylelint-config-standard', 'stylelint-config-prettier'].map(require.resolve),
+
+  rules: {
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: ['value'],
+      },
+    ],
+
+    'selector-pseudo-class-no-unknown': [
+      true,
+      {
+        ignorePseudoClasses: ['global'],
+      },
+    ],
+
+    'property-no-unknown': [
+      true,
+      {
+        ignoreProperties: ['composes'],
+      },
+    ],
+
+    'max-nesting-depth': 3,
+  },
 };
