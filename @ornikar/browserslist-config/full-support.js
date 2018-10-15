4ff406b27ac2733cfddf3b877677d674db7e7b69
Christophe Hurpeau <christophe.hurpeau+github@gmail.com>
2018-10-15 12:37:26 +0200

    chore: update prettier config (#21)
    BREAKING CHANGE: prettier@^1.14.3, eslint-plugin-prettier@^3.0.0

--- @ornikar/browserslist-config/full-support.js
+++ @ornikar/browserslist-config/full-support.js
@@ -2,4 +2,6 @@
 
 // Note: if you update this, update the link in README.md !
 
-module.exports = 'last 4 versions, > 1% in FR, not ie < 12, Firefox ESR'.split(',');
+module.exports = 'last 4 versions, > 1% in FR, not ie < 12, Firefox ESR'.split(
+  ','
+);
