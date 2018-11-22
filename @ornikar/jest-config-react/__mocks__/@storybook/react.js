1dbcf08411e5dc57f29fea81934a0dab53cc6eb2
Christophe Hurpeau <christophe.hurpeau+github@gmail.com>
2018-11-22 10:52:18 +0100

    feat: jest config stories (#30)
    * feat: jest config stories
    
    * fix: disable lifecyle methods

---
+++ @ornikar/jest-config-react/__mocks__/@storybook/react.js
@@ -0,0 +1,33 @@
+/* eslint-env jest */
+
+'use strict';
+
+const { shallow } = require('enzyme');
+
+// Mocked version of `import { action } from '@storybook/react'`.
+exports.action = actionName => jest.fn();
+
+// Mocked version of: `import { storiesOf } from '@storybook/react'`
+exports.storiesOf = groupName => {
+  // Mocked API to generate tests from & snapshot stories.
+  const api = {
+    add(storyName, story) {
+      describe(groupName, () => {
+        it(storyName, () => {
+          expect(
+            shallow(story(), { disableLifecycleMethods: true })
+          ).toMatchSnapshot();
+        });
+      });
+
+      return api;
+    },
+
+    // Any `storybook-addon-*` packages may require noop-ing them:
+    addDecorator() {
+      return api;
+    },
+  };
+
+  return api;
+};
