dc90483f941f7b6e9b895d304b6764fa23815ccb
Christophe Hurpeau <christophe.hurpeau+github@gmail.com>
2018-11-26 13:36:47 +0100

    feat: add default test match (#31)

--- @ornikar/jest-config-react/jest-preset.js
+++ @ornikar/jest-config-react/jest-preset.js
@@ -4,6 +4,7 @@ const baseJestPreset = require('@ornikar/jest-config/jest-preset');
 
 module.exports = {
   ...baseJestPreset,
+  testMatch: [...baseJestPreset.testMatch, '<rootDir>/src/**/stories.js'],
   snapshotSerializers: ['enzyme-to-json/serializer'],
   setupFiles: [
     baseJestPreset.setupFiles[0],
--- @ornikar/jest-config/jest-preset.js
+++ @ornikar/jest-config/jest-preset.js
@@ -2,6 +2,10 @@
 
 module.exports = {
   cacheDirectory: './node_modules/.cache/jest',
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.js',
+    '<rootDir>/src/**/*.test.js',
+  ],
   setupTestFrameworkScriptFile: require.resolve('./test-setup-framework.js'),
   setupFiles: [require.resolve('./test-shim.js'), '<rootDir>/test-setup.js'],
 };
