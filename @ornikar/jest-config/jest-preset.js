7faf497b6d67bef635b6547ea18cf288ff9ab6a3
Christophe Hurpeau <christophe@hurpeau.com>
2019-02-26 11:52:19 +0100

    feat: jest config add support typescript

--- @ornikar/jest-config/jest-preset.js
+++ @ornikar/jest-config/jest-preset.js
@@ -1,10 +1,15 @@
 'use strict';
 
+const fs = require('fs');
+const path = require('fs');
+
+const useTypescript = fs.existsSync(path.resolve('tsconfig.json'));
+
 module.exports = {
   cacheDirectory: './node_modules/.cache/jest',
   testMatch: [
-    '<rootDir>/src/**/__tests__/**/*.js',
-    '<rootDir>/src/**/*.test.js',
+    `<rootDir>/src/**/__tests__/**/*.${useTypescript ? 'ts?(x)' : 'js'}`,
+    `<rootDir>/src/**/*.test.${useTypescript ? 'ts?(x)' : 'js'}`,
   ],
   setupTestFrameworkScriptFile: require.resolve('./test-setup-framework.js'),
   setupFiles: [require.resolve('./test-shim.js'), '<rootDir>/test-setup.js'],
