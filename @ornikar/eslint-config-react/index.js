7c7741919e8fc69a68af87db22e52ef2cf8f7f0e
Christophe Hurpeau <christophe.hurpeau+github@gmail.com>
2019-04-10 11:05:22 +0200

    feat: enable react-hooks/exhaustive-deps [no issue] (#137)

--- @ornikar/eslint-config-react/index.js
+++ @ornikar/eslint-config-react/index.js
@@ -31,5 +31,6 @@ module.exports = {
     'react/jsx-filename-extension': ['error', { extensions: ['js'] }],
 
     'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'error'
   },
 };
