e175492a8f69bf2f3850f44b5c38762615ec4b34
Corentin André <corentin7andre8@gmail.com>
2019-04-15 18:13:30 +0200

    fix: support date parse function in jest

--- @ornikar/jest-config/global-mocks.js
+++ @ornikar/jest-config/global-mocks.js
@@ -2,10 +2,14 @@
 
 const timezoneMock = require('timezone-mock');
 
+const { parse } = global.Date;
 timezoneMock.register('UTC');
 
 const MockDate = global.Date;
 // 2018-07-30T08:52:42.679Z
 const mockNowTime = 1532940762679;
 MockDate.now = () => mockNowTime;
+// Fix issue with parse not being overriden in timezone-mock.
+// A pr is opened at timezone-mock to get rid of this hack https://github.com/Jimbly/timezone-mock/pull/21
+MockDate.parse = parse;
 MockDate.getFullYear = () => 2019;
