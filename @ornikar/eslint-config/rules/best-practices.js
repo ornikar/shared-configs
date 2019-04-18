8b087da326dc1d5e2b82d1d4cea8356dc5b523f4
Julien Tilap LA VINH <jlavinh@gmail.com>
2019-04-18 17:35:52 +0200

    feat(eslintconfig): rule no-warning-comments

--- @ornikar/eslint-config/rules/best-practices.js
+++ @ornikar/eslint-config/rules/best-practices.js
@@ -4,5 +4,8 @@ module.exports = {
   rules: {
     // https://eslint.org/docs/rules/require-await
     'require-await': 'error',
+
+    // https://eslint.org/docs/2.0.0/rules/no-warning-comments
+    'no-warning-comments': 'error',
   },
 };
