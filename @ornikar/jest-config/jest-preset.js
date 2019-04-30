99feb5ae6c9040ee1c8078c9995cab27a5ea2ec3
Christophe Hurpeau <christophe@hurpeau.com>
2019-04-30 14:52:59 +0200

    feat: allow both js and ts tests [no issue] (#164)

--- @ornikar/jest-config/jest-preset.js
+++ @ornikar/jest-config/jest-preset.js
@@ -8,8 +8,8 @@ const useTypescript = fs.existsSync(path.resolve('tsconfig.json'));
 module.exports = {
   cacheDirectory: './node_modules/.cache/jest',
   testMatch: [
-    `<rootDir>/src/**/__tests__/**/*.${useTypescript ? 'ts?(x)' : 'js'}`,
-    `<rootDir>/src/**/*.test.${useTypescript ? 'ts?(x)' : 'js'}`,
+    `<rootDir>/src/**/__tests__/**/*.${useTypescript ? '{js,ts,tsx}' : 'js'}`,
+    `<rootDir>/src/**/*.test.${useTypescript ? '{js,ts,tsx}' : 'js'}`,
   ],
   setupFilesAfterEnv: [require.resolve('./test-setup-after-env.js')],
   setupFiles: [
