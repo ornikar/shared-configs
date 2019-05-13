37d6dc0894bba49197b1d2ae0b77db8e9000630c
Christophe Hurpeau <christophe@hurpeau.com>
2019-05-13 16:50:38 +0200

    feat: enable import/no-anonymous-default-export for functions [no issue] (#172)

--- @ornikar/eslint-config-babel-use/index.js
+++ @ornikar/eslint-config-babel-use/index.js
@@ -22,5 +22,24 @@ module.exports = {
     // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/import/no-extraneous-dependencies.md
     // override default airbnb exceptions
     'import/no-extraneous-dependencies': ['error', { devDependencies: false }],
+
+    // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-anonymous-default-export.md
+    // Empeche de créer des composants et de les exporter sans les nommer.
+    // Exemple: `export default () => { return <Logo color="black" />; };`
+    // Ce qu'il faut faire: `export default function BlackLogo() { return <Logo color="black" /> };`
+    // Lorsque des componsants n'ont pas de noms, react nous envoie des erreurs avec une stack pas explicite.
+    // Les noms disparaissent lors de la minification du bundle, lorsqu'il ne sont pas utilisés et que ce sont des default.
+    'import/no-anonymous-default-export': [
+      'error',
+      {
+        allowArray: true,
+        allowArrowFunction: false,
+        allowAnonymousClass: false,
+        allowAnonymousFunction: false,
+        allowCallExpression: true,
+        allowLiteral: true,
+        allowObject: true,
+      },
+    ],
   },
 };
