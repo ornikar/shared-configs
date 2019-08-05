66823f21648586920df34257642d546bead61dbe
Christophe Hurpeau <christophe.hurpeau+github@gmail.com>
2019-08-05 10:44:56 +0200

    Revert "fix: dont use remove rollup plugin ignore import dep & usage (#245)"
    This reverts commit aa2c141a332c6b3ffdfa4330fb3eaf7590dd14ad.

--- @ornikar/rollup-config/createRollupConfig.js
+++ @ornikar/rollup-config/createRollupConfig.js
@@ -8,12 +8,14 @@ const postcss = require('rollup-plugin-postcss');
 const babel = require('rollup-plugin-babel');
 const resolve = require('rollup-plugin-node-resolve');
 const commonjs = require('rollup-plugin-commonjs');
+const ignoreImport = require('rollup-plugin-ignore-import');
 const configExternalDependencies = require('rollup-config-external-dependencies');
 
 // eslint-disable-next-line import/no-dynamic-require
 const rootPkg = require(path.resolve('./package.json'));
 
 const extensions = ['.js', '.jsx', '.tsx', '.ts'];
+const browserOnlyExtensions = ['.css'];
 
 const createBuildsForPackage = (packagesDir, packageName) => {
   // eslint-disable-next-line import/no-dynamic-require, global-require
@@ -46,16 +48,20 @@ const createBuildsForPackage = (packagesDir, packageName) => {
       external: target === 'node' ? (filePath) => (filePath.endsWith('.css') ? false : external(filePath)) : external,
 
       plugins: [
-        postcss({
-          extract: exportCss ? `${distPath}/styles.css` : true,
-          modules: true,
-          config: exportCss
-            ? {
-                path: path.resolve('./config/rollup-postcss.config'),
-              }
-            : false,
-          minimize: false,
-        }),
+        target === 'browser'
+          ? postcss({
+              extract: exportCss ? `${distPath}/styles.css` : true,
+              modules: true,
+              config: exportCss
+                ? {
+                    path: path.resolve('./config/rollup-postcss.config'),
+                  }
+                : false,
+              minimize: false,
+            })
+          : ignoreImport({
+              extensions: browserOnlyExtensions,
+            }),
         babel({
           babelrc: false,
           configFile: true,
