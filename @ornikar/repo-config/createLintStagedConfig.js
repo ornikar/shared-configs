6274e03bdcab181b2b94445a0627a50cb1e527a8
Corentin André <corentin7andre8@gmail.com>
2019-08-14 18:15:49 +0200

    fix: detect presence of prettier or stylelint in project devdeps
    conditionnally apply lint staged commands depending on presence of node modules

--- @ornikar/repo-config/createLintStagedConfig.js
+++ @ornikar/repo-config/createLintStagedConfig.js
@@ -12,19 +12,21 @@ const getSrcDirectories = (srcDirectoryName = 'src') =>
 module.exports = function createLintStagedConfig(options = {}) {
   const srcExtensions = options.srcExtensions || ['js'];
   const srcDirectories = getSrcDirectories(options.srcDirectoryName);
-
+  const hasPrettier = pkg.devDependencies && pkg.devDependencies.prettier;
+  const hasStyleLint = pkg.devDependencies && pkg.devDependencies.stylelint;
+  const jsonFiles = `{.eslintrc.json,package.json${
+    workspaces
+      ? `,${workspaces.map((workspacePath) => `${workspacePath}/{.eslintrc.json,package.json}`).join(',')}`
+      : ''
+  }`;
   return {
     'yarn.lock': ['yarn-update-lock', 'git add'],
-    [`{.eslintrc.json,package.json${
-      workspaces
-        ? `,${workspaces.map((workspacePath) => `${workspacePath}/{.eslintrc.json,package.json}`).join(',')}`
-        : ''
-    }`]: ['prettier --parser json --write', 'git add'],
+    [jsonFiles]: [hasPrettier && 'prettier --parser json --write', 'git add'].filter(Boolean),
     [`{.storybook,${srcDirectories}}/**/*.css`]: [
-      'prettier --parser css --write',
-      'stylelint --quiet --fix',
+      hasPrettier && 'prettier --parser css --write',
+      hasStyleLint && 'stylelint --quiet --fix',
       'git add',
-    ],
+    ].filter(Boolean),
 
     [`${srcDirectories}/**/*.{${srcExtensions.join(',')}}`]: ['eslint --fix --quiet', 'git add'],
     '{scripts,config,.storyboook}/*.js': ['eslint --fix --quiet', 'git add'],
