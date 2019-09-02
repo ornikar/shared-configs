469b49f3e47d3cfebeb12878f49beb7e358ac9a7
Valérian Brie <valerian@ornikar.com>
2019-09-02 12:13:31 +0200

    chore: execute typescript css linter if typescript exists in project

--- @ornikar/repo-config-react/createLintStagedConfig.js
+++ @ornikar/repo-config-react/createLintStagedConfig.js
@@ -1,6 +1,10 @@
 'use strict';
 
+const path = require('path');
 const createBaseLintStagedConfig = require('@ornikar/repo-config/createLintStagedConfig');
+
+// eslint-disable-next-line import/no-dynamic-require
+const pkg = require(path.resolve('package.json'));
 
 module.exports = function createLintStagedConfig(options = {}) {
   const config = createBaseLintStagedConfig({ srcExtensions: ['js', 'ts', 'tsx'] });
@@ -8,13 +12,17 @@ module.exports = function createLintStagedConfig(options = {}) {
   // eslint-disable-next-line prefer-destructuring
   const srcDirectories = createBaseLintStagedConfig.getSrcDirectories();
 
-  Object.assign(config, {
-    [`${srcDirectories}/**/*.module.{css,css.d.ts}`]: (filenames) => [
+  const additionalConfig = {
+    '*.svg': ['svgo --multipass --config=node_modules/@ornikar/repo-config-react/.svgo.yml', 'git add'],
+  };
+  if (pkg.devDependencies.typescript) {
+    additionalConfig[`${srcDirectories}/**/*.module.{css,css.d.ts}`] = (filenames) => [
       "tcm -s -p '**/*.module.css'",
       "git add '**/**.d.ts'",
-    ],
-    '*.svg': ['svgo --multipass --config=node_modules/@ornikar/repo-config-react/.svgo.yml', 'git add'],
-  });
+    ];
+  }
+
+  Object.assign(config, additionalConfig);
 
   return config;
 };
