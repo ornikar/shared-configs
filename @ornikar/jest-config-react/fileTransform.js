904d0d60b5777e2fca17c0396db189853fc5ee45
Christophe Hurpeau <christophe@hurpeau.com>
2019-09-30 10:16:13 +0200

    fix: svg transform [no issue] (#269)

---
+++ @ornikar/jest-config-react/fileTransform.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const path = require('path');
+
+module.exports = {
+  process(src, filename) {
+    const assetFilename = JSON.stringify(path.basename(filename));
+    return `module.exports = ${assetFilename};`;
+  },
+};
