69231ec1fb07d9bbe93ecd1015d8e2a73f5307ff
Christophe Hurpeau <christophe@hurpeau.com>
2019-10-09 09:33:38 +0200

    fix(jest-config-react): preset for non craco configs

--- @ornikar/jest-config-react/jest-preset.js
+++ @ornikar/jest-config-react/jest-preset.js
@@ -1,6 +1,10 @@
 'use strict';
 
+const fs = require('fs');
+const path = require('path');
 const baseJestPreset = require('@ornikar/jest-config/jest-preset');
+
+const useCraco = fs.existsSync(path.resolve('craco.config.js'));
 
 module.exports = {
   ...baseJestPreset,
@@ -11,9 +15,15 @@ module.exports = {
     require.resolve('./test-setup'),
     baseJestPreset.setupFiles[baseJestPreset.setupFiles.length - 1],
   ],
-  transform: {
-    '\\.svg$': require.resolve('./fileTransform'),
-  },
+  transform: useCraco
+    ? {
+        '\\.svg$': require.resolve('./fileTransform'),
+      }
+    : {
+        '\\.svg$': require.resolve('./fileTransform'),
+        '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
+      },
+  transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|jsx|ts|tsx)$', '^.+\\.css$'],
   moduleNameMapper: {
     '\\.css$': 'identity-obj-proxy',
     '@storybook/react$': require.resolve('./__mocks__/@storybook/react'),
