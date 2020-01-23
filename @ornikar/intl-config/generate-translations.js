fc8c73f38ee908fbca7fdd252ffebb4703f57f74
Stefan <stefan.lentner@gmail.com>
2020-01-23 18:07:28 +0100

    feat: generate-trasnlations script

--- @ornikar/intl-config/generate-translations.js
+++ @ornikar/intl-config/generate-translations.js
@@ -1,5 +1,4 @@
-#!/usr/bin/env node
-/* eslint-disable import/no-extraneous-dependencies, node/no-extraneous-require, no-param-reassign, node/no-missing-require */
+/* eslint-disable no-param-reassign */
 
 'use strict';
 
@@ -7,19 +6,20 @@ const fs = require('fs');
 const path = require('path');
 const globSync = require('glob').sync;
 const babelCore = require('@babel/core');
+const babelPluginReactIntl = require('babel-plugin-react-intl');
 
 process.env.NODE_ENV = 'production';
 
-module.exports = (paths) => {
+module.exports = ({ paths, babelConfig }) => {
   paths.forEach(({ name, messageGlob, distDir }) => {
-    const defaultMessages = globSync(messageGlob, { ignore: '**/*.module.css.d.ts' })
+    const defaultMessages = globSync(messageGlob, { ignore: ['**/*.module.css.d.ts', '**/stories.{ts,tsx}'] })
       .map((filename) => ({ filename, code: fs.readFileSync(filename, 'utf8') }))
       .map(
         ({ filename, code }) =>
           babelCore.transformSync(code, {
             filename,
-            presets: [require.resolve('babel-preset-react-app')],
-            plugins: [require.resolve('babel-plugin-react-intl')],
+            ...babelConfig,
+            plugins: [babelPluginReactIntl, ...babelConfig.plugins],
           }).metadata['react-intl'].messages,
       )
       .reduce((collection, descriptors) => {
@@ -35,10 +35,7 @@ module.exports = (paths) => {
     const destinationFile = path.join(distDir, `${name ? `fr-FR/${name}` : 'fr-FR'}.json`);
     const destinationFolder = path.dirname(destinationFile);
 
-    if (!fs.existsSync(destinationFolder)) {
-      fs.mkdirSync(destinationFolder, { recursive: true });
-    }
-
+    fs.mkdirSync(destinationFolder, { recursive: true });
     fs.writeFileSync(destinationFile, JSON.stringify(defaultMessages, null, 2));
   });
 };
