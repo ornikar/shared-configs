f3148fa0726eb326d51e3059d52ed9a234379a6c
Corentin André <corentin7andre8@gmail.com>
2020-02-11 15:25:46 +0100

    fix: change import order

--- @ornikar/jest-config/test-shim.js
+++ @ornikar/jest-config/test-shim.js
@@ -1,10 +1,10 @@
 'use strict';
 
 /* Always load Intl Polyfill */
-require('@formatjs/intl-pluralrules/polyfill');
-require('@formatjs/intl-pluralrules/dist/locale-data/fr');
 const IntlPolyfill = require('intl');
 require('intl/locale-data/jsonp/fr');
+require('@formatjs/intl-pluralrules/polyfill');
+require('@formatjs/intl-pluralrules/dist/locale-data/fr');
 
 global.Intl = IntlPolyfill;
 global.NumberFormat = IntlPolyfill.NumberFormat;
