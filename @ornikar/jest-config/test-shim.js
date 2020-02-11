a91acfda332ce4de8d0be014a6735b9ea92da535
Corentin.Andre <corentin7andre8@gmail.com>
2020-02-11 15:27:39 +0100

    feat: add polyfill for plural rules [no issue] (#324)
    * feat: add polyfill for plural rules
    
    * fix: change import order

--- @ornikar/jest-config/test-shim.js
+++ @ornikar/jest-config/test-shim.js
@@ -3,6 +3,8 @@
 /* Always load Intl Polyfill */
 const IntlPolyfill = require('intl');
 require('intl/locale-data/jsonp/fr');
+require('@formatjs/intl-pluralrules/polyfill');
+require('@formatjs/intl-pluralrules/dist/locale-data/fr');
 
 global.Intl = IntlPolyfill;
 global.NumberFormat = IntlPolyfill.NumberFormat;
