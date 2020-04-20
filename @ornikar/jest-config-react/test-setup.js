9c79811c43165bbc2b4d81bbf767d2d7b336b321
Maxime Martin <maxime@ornikar.com>
2020-04-20 10:50:46 +0200

    feat: jest mock portal for testing library

--- @ornikar/jest-config-react/test-setup.js
+++ @ornikar/jest-config-react/test-setup.js
@@ -1,6 +1,18 @@
+/* eslint-env jest */
+
 'use strict';
 
 const { configure } = require('enzyme');
 const Adapter = require('enzyme-adapter-react-16');
+
+// Usefull to display snapshot of portals (drawer or modal for example) with react testing library
+jest.mock('react-dom', () => {
+  return {
+    ...jest.requireActual('react-dom'),
+    createPortal: (element) => {
+      return element;
+    },
+  };
+});
 
 configure({ adapter: new Adapter() });
