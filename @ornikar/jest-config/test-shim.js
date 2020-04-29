45bf7ab787976a6b40f0862185f163cc1a04c77c
Christophe Hurpeau <christophe@hurpeau.com>
2020-04-29 10:01:59 +0200

    feat: remove intl polyfill
    Since node 12, we no longer need polyfills, just node-icu to load fr data
    
    BREAKING CHANGE: project should add a devdep to node-icu and run commands with NODE_ICU_DATA env variable

--- @ornikar/jest-config/test-shim.js
+++ @ornikar/jest-config/test-shim.js
@@ -1,17 +1,5 @@
 'use strict';
 
-/* Always load Intl Polyfill */
-const IntlPolyfill = require('intl');
-require('intl/locale-data/jsonp/fr');
-
-global.Intl = IntlPolyfill;
-// order matters because formatjs polyfill changes global Intl object
-require('@formatjs/intl-pluralrules/polyfill');
-require('@formatjs/intl-pluralrules/dist/locale-data/fr');
-
-global.NumberFormat = IntlPolyfill.NumberFormat;
-global.DateTimeFormat = IntlPolyfill.DateTimeFormat;
-
 global.requestAnimationFrame = (callback) => {
   setTimeout(callback, 0);
 };
