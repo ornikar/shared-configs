43d93f5496b85069f70b44b2afb2ff5d75d1c7dd
Christophe Hurpeau <christophe@hurpeau.com>
2020-05-20 14:38:10 +0200

    feat: avoir typing when expression typed (#374)

--- @ornikar/eslint-config-typescript/plugins/typescript-eslint.js
+++ @ornikar/eslint-config-typescript/plugins/typescript-eslint.js
@@ -110,7 +110,7 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': [
       'warn',
       {
-        allowExpressions: false,
+        allowExpressions: true,
         allowTypedFunctionExpressions: true,
         allowHigherOrderFunctions: true,
       },
