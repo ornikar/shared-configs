8207b7e6918db285293db6f5accb558b087d0a47
Christophe Hurpeau <christophe@hurpeau.com>
2020-07-10 17:39:41 +0200

    fix: use ignore to allow Typography.{tag}

--- @ornikar/eslint-config-react/rules/react.js
+++ @ornikar/eslint-config-react/rules/react.js
@@ -94,6 +94,15 @@ module.exports = {
       },
     ],
 
+    // // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-pascal-case.md
+    'react/jsx-pascal-case': [
+      'error',
+      {
+        // Allow Typography.{tag}
+        ignore: ['h1', 'h2', 'h3', 'h4', 'h5', 'div', 'span', 'p', 'section'],
+      },
+    ],
+
     /* disabled rules */
 
     // https://github.com/yannickcr/eslint-plugin-react/issues/1009
@@ -115,9 +124,5 @@ module.exports = {
 
     // allow props spreading
     'react/jsx-props-no-spreading': 'off',
-
-    // disabled until this issue is resolved:
-    // https://github.com/yannickcr/eslint-plugin-react/issues/1334
-    'react/jsx-pascal-case': 'off',
   },
 };
