82b19f1e6c0aa96835ba3ab612655af62ed3a1ae
Christopher <christopher@ornikar.com>
2020-07-28 09:52:48 +0200

    feat(eslint-config): remove parserOptions from graphql config

--- @ornikar/eslint-config/graphql.js
+++ @ornikar/eslint-config/graphql.js
@@ -1,10 +1,6 @@
 'use strict';
 
 module.exports = {
-  parserOptions: {
-    sourceType: 'script',
-  },
-
   rules: {
     // https://eslint.org/docs/rules/no-underscore-dangle
     'no-underscore-dangle': [
