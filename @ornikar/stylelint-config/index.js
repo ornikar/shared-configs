f9baaa33d988cf6965030e9f7919f9bc3cfe941d
Christophe Hurpeau <christophe@hurpeau.com>
2020-09-04 14:03:21 +0200

    fix(stylelint-config): allow composes to have any case, allow font-family with inherit value

--- @ornikar/stylelint-config/index.js
+++ @ornikar/stylelint-config/index.js
@@ -13,10 +13,17 @@ module.exports = {
 
     'order/order': ['declarations', 'rules', 'at-rules'],
 
+    // override default rule to disable for composes, used in css modules
+    // https://stylelint.io/user-guide/rules/value-keyword-case
+    'value-keyword-case': ['lower', { ignoreProperties: ['composes'] }],
+
     // https://stylelint.io/user-guide/rules/comment-word-blacklist/
     'comment-word-blacklist': ['/^TODO:/', '/^FIXME:/'],
 
     // https://stylelint.io/user-guide/rules/property-blacklist/
-    'property-blacklist': ['font-size', 'font-family'],
+    'property-blacklist': ['font-size'],
+
+    // https://stylelint.io/user-guide/rules/declaration-property-value-whitelist/
+    'declaration-property-value-whitelist': { 'font-family': ['inherit'] },
   },
 };
