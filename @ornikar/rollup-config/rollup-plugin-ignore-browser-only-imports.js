f2f6b6ead18be83575217e4d455564197bb00c21
Christophe Hurpeau <christophe@hurpeau.com>
2021-03-02 14:50:51 +0100

    feat(rollup-config): rollup 2

---
+++ @ornikar/rollup-config/rollup-plugin-ignore-browser-only-imports.js
@@ -0,0 +1,17 @@
+/* eslint-disable filenames/match-exported */
+
+'use strict';
+
+const emptyFile = 'export default {}';
+const emptyFileName = '\0rollup_plugin_ignoreBrowserOnlyImports_empty_module_placeholder';
+
+module.exports = function ignoreBrowserOnlyImports({ extensions, exclude }) {
+  return {
+    resolveId(importee) {
+      return extensions.some((ext) => importee.endsWith(ext) && !exclude.test(importee)) ? emptyFileName : null;
+    },
+    load(id) {
+      return id === emptyFileName ? emptyFile : null;
+    },
+  };
+};
