12325b5569a8894451def6a12ac5e5efeeeb18f2
Christophe Hurpeau <christophe@hurpeau.com>
2021-09-27 07:14:23 +0200

    feat(repo-config): upgrade to husky v7
    BREAKING CHANGE: requires to remove husky devDependency and add postinstall script "ornikar-repo-config-postinstall" in package.json

---
+++ @ornikar/repo-config/bin/ornikar-repo-config-postinstall.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const whichPmRuns = require('which-pm-runs');
+
+if (!process.env.INIT_CWD) {
+  console.error('Missing process.env.INIT_CWD. Did you use postinstall script ?');
+  process.exit(1);
+}
+
+process.chdir(process.env.PROJECT_CWD || process.env.INIT_CWD);
+
+const pm = whichPmRuns();
+
+if (!pm) {
+  console.error('Invalid package manager, please run with postinstall hook!');
+  process.exit(1);
+}
+
+const pkg = JSON.parse(fs.readFileSync(path.resolve('package.json')));
+
+require('../lib/postinstall/install-husky.js')({ pkg, pm });
--- husky.config.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-const createHuskyConfig = require('./@ornikar/repo-config/createHuskyConfig');
-
-module.exports = createHuskyConfig();
