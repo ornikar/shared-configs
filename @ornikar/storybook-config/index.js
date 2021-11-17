0039ce593b559a296eb55530c0a5179ca022c011
Corentin André <corentin7andre8@gmail.com>
2021-11-17 18:57:31 +0100

    fix: do not apply postcss addon if cra preset is present

--- @ornikar/storybook-config/index.js
+++ @ornikar/storybook-config/index.js
@@ -8,32 +8,36 @@ exports.createMainConfig = function createMainConfig({
   enableControls = false,
   addons = [],
 } = {}) {
+  const addonsList = [
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: postcssImplementation,
+          postcssOptions: {
+            config: path.resolve('./.storybook/postcss.config.js'),
+          },
+        },
+      },
+    },
+    {
+      name: '@storybook/addon-essentials',
+      options: {
+        controls: enableControls,
+      },
+    },
+    ...addons,
+  ];
+
+  // When cra preset is already installed, we should not have postcss addon
+  if (addonsList.includes('@storybook/preset-create-react-app')) {
+    addonsList.splice(0, 1);
+  }
   return {
     typescript: {
       check: false,
     },
     stories: [`../${srcPath}/**/@(stories.ts?(x)|stories.ts?(x))`],
-    addons: [
-      {
-        name: '@storybook/addon-postcss',
-        options: {
-          postcssLoaderOptions: {
-            implementation: postcssImplementation,
-            postcssOptions: {
-              config: path.resolve('./.storybook/postcss.config.js'),
-            },
-          },
-        },
-      },
-
-      {
-        name: '@storybook/addon-essentials',
-        options: {
-          controls: enableControls,
-        },
-      },
-
-      ...addons,
-    ],
+    addons: addonsList,
   };
 };
