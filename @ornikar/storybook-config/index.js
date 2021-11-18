9c70741fea6f8ab14203c48db9cfbecaeb6ee4ae
Christophe Hurpeau <christophe@hurpeau.com>
2021-11-18 10:41:09 +0100

    refactor: use .filter(Boolean)

--- @ornikar/storybook-config/index.js
+++ @ornikar/storybook-config/index.js
@@ -8,36 +8,33 @@ exports.createMainConfig = function createMainConfig({
   enableControls = false,
   addons = [],
 } = {}) {
-  const addonsList = [
-    {
-      name: '@storybook/addon-postcss',
-      options: {
-        postcssLoaderOptions: {
-          implementation: postcssImplementation,
-          postcssOptions: {
-            config: path.resolve('./.storybook/postcss.config.js'),
-          },
-        },
-      },
-    },
-    {
-      name: '@storybook/addon-essentials',
-      options: {
-        controls: enableControls,
-      },
-    },
-    ...addons,
-  ];
-
-  // When cra preset is already installed, we should not have postcss addon
-  if (addonsList.includes('@storybook/preset-create-react-app')) {
-    addonsList.splice(0, 1);
-  }
   return {
     typescript: {
       check: false,
     },
     stories: [`../${srcPath}/**/@(stories.ts?(x)|stories.ts?(x))`],
-    addons: addonsList,
+    addons: [
+      // When cra preset is already installed, we should not have postcss addon
+      addons.includes('@storybook/preset-create-react-app')
+        ? null
+        : {
+            name: '@storybook/addon-postcss',
+            options: {
+              postcssLoaderOptions: {
+                implementation: postcssImplementation,
+                postcssOptions: {
+                  config: path.resolve('./.storybook/postcss.config.js'),
+                },
+              },
+            },
+          },
+      {
+        name: '@storybook/addon-essentials',
+        options: {
+          controls: enableControls,
+        },
+      },
+      ...addons,
+    ].filter(Boolean),
   };
 };
