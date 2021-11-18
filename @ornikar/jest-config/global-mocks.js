bf1ac619aaf785fff2f3328a73934bcc74a64a6b
Christophe Hurpeau <christophe@hurpeau.com>
2021-11-18 17:27:24 +0100

    feat: drop timezone-mock in favor of env variable [no issue] (#534)
    BREAKING CHANGE: you should add TZ env variable `"test": "TZ='UTC' jest"`

--- @ornikar/jest-config/global-mocks.js
+++ @ornikar/jest-config/global-mocks.js
@@ -1,15 +1,8 @@
 'use strict';
 
-// order is: mockdate, then timezone mock
-// requiring timezone mock sets its internal _Date, so date must be mocked before.
-// eslint-disable-next-line import/order
 const mockdate = require('mockdate');
 
 mockdate.set('2018-07-30T08:52:42.679Z');
-
-const timezoneMock = require('timezone-mock');
-
-timezoneMock.register('UTC');
 
 // Mock getFullYear
 Date.getFullYear = () => 2019;
