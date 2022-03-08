31f7a63d05d3b171833f96931c5dee8ab896ba70
Christophe Hurpeau <christophe@hurpeau.com>
2022-03-08 12:07:58 +0100

    refactor(jest-config-react-native): cleanup config

--- @ornikar/jest-config-react-native/jest-preset.js
+++ @ornikar/jest-config-react-native/jest-preset.js
@@ -1,24 +1,19 @@
 'use strict';
 
-const jestPreset = require('@ornikar/jest-config/jest-preset');
+const baseOrnikarPreset = require('@ornikar/jest-config/jest-preset');
 const expoPreset = require('jest-expo/jest-preset');
-const mergeWith = require('lodash.mergewith');
-
-function customizer(objValue, srcValue) {
-  if (Array.isArray(objValue)) {
-    // eslint-disable-next-line unicorn/prefer-spread
-    return objValue.concat(srcValue);
-  }
-  return undefined;
-}
-
-const basePreset = mergeWith(expoPreset, jestPreset, customizer);
 
 module.exports = {
-  ...basePreset,
-  testMatch: [...basePreset.testMatch, '<rootDir>/src/**/stories.{ts,tsx}', '<rootDir>/src/**/*.stories.{ts,tsx}'],
+  ...baseOrnikarPreset,
+  ...expoPreset,
+  setupFiles: [...expoPreset.setupFiles, ...baseOrnikarPreset.setupFiles],
+  testMatch: [
+    ...baseOrnikarPreset.testMatch,
+    '<rootDir>/src/**/stories.{ts,tsx}',
+    '<rootDir>/src/**/*.stories.{ts,tsx}',
+  ],
   moduleNameMapper: {
-    ...basePreset.moduleNameMapper,
+    ...expoPreset.moduleNameMapper,
     '^@storybook/addon-actions$': require.resolve('./__mocks__/@storybook/addon-actions.js'),
     '@storybook/react-native$': require.resolve('./__mocks__/@storybook/react-native.jsx'),
     '^@storybook/react-native$': require.resolve('./__mocks__/@storybook/react-native.jsx'),
@@ -28,7 +23,7 @@ module.exports = {
   transform: {
     // remove svg asset transformer from expo config, as we configure svg with custom metro transformer
     ...Object.fromEntries(
-      Object.entries(basePreset.transform).map(([key, value]) => {
+      Object.entries(expoPreset.transform).map(([key, value]) => {
         if (key.includes('|svg|')) return [key.replace('|svg|', '|'), value];
         return [key, value];
       }),
