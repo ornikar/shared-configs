41fa658ab298912eb1285d0973776d4dd01334a1
Christophe Hurpeau <christophe@hurpeau.com>
2022-03-31 11:45:46 +0200

    refactor: typo

--- @ornikar/jest-config-react-native/customTransforms.js
+++ @ornikar/jest-config-react-native/customTransforms.js
@@ -1,7 +1,7 @@
 'use strict';
 
 exports.customTransforms = {
-  // compilation of problematic node_modules has a simplier babel config
+  // compilation of problematic node_modules has a simpler babel config
   'node_modules/(react-native-(calendars|reanimated)|native-base|@react-native-community/netinfo)/.*\\.(js|jsx|ts|tsx)$':
     require.resolve('./transformers/babel-transformer-node-modules.js'),
 
@@ -11,6 +11,6 @@ exports.customTransforms = {
   // compilation of most node_modules with sucrase for faster setup
   'node_modules/(@?react-native.*|@?expo.*|@?react-navigation.*)/.*\\.(js|jsx|ts|tsx)$': '@sucrase/jest-plugin',
 
-  // compilation of rest node_modules has a simplier babel config (might be additional transformIgnorePatterns)
+  // compilation of rest node_modules has a simpler babel config (might be additional transformIgnorePatterns)
   'node_modules.*\\.(js|jsx|ts|tsx)$': require.resolve('./transformers/babel-transformer-node-modules.js'),
 };
