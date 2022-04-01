cc414824170d71b1aff10693983385caa0bc58ed
Christophe Hurpeau <christophe@hurpeau.com>
2022-04-01 17:23:51 +0200

    feat(repo-config): reexport commitlint for yarn berry

---
+++ @ornikar/repo-config/bin/commitlint.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const path = require('path');
+const pkg = require('@commitlint/cli/package.json');
+
+// eslint-disable-next-line import/no-dynamic-require
+require(path.join('@commitlint/cli', typeof pkg.bin === 'string' ? pkg.bin : pkg.bin.commitlint));
