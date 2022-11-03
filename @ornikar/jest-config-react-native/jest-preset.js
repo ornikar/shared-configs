ornikar/shared-configs

commit e1f17719e2b925de915990cafa94dd4819ed32d5

feat(lerna-config): integrate lerna cli in lerna-config ARCH-1591 (#801)
BREAKING CHANGE: requires to remove direct lerna in devDependencies

@ornikar/jest-config-react-native/jest-preset.js
@@ -19,7 +19,7 @@ module.exports = {
   },
   // override expo transformIgnorePatterns with custom config
   transformIgnorePatterns: [
-    'node_modules/(?!react-native|@react-native|expo|@expo(nent)?/.*|react-navigation|@react-navigation/.*|native-base)',
+    'node_modules/(?!(react-native|@react-native|expo|@expo(nent)?/.*|react-navigation|@react-navigation/.*|native-base)/)',
   ],
   transform: {
     ...customTransforms,

commit bf74ac295ce812ffe8f123b7ce19353d8a8bd077

fix: fix transformIgnorePatterns bug introduced in #801

@ornikar/jest-config-react-native/jest-preset.js
@@ -19,7 +19,7 @@ module.exports = {
   },
   // override expo transformIgnorePatterns with custom config
   transformIgnorePatterns: [
-    'node_modules/(?!(react-native|@react-native|expo|@expo(nent)?/.*|react-navigation|@react-navigation/.*|native-base)/)',
+    'node_modules/(?!(react-native.*|@react-native.*|expo.*|@expo(nent)?/.*|react-navigation.*|@react-navigation/.*|native-base)/)',
   ],
   transform: {
     ...customTransforms,