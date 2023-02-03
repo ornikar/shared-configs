11fe9deb3047460569fad0e8d9fd86647f5907d0
Arnaud Manaranche <arnaud.manaranche@ornikar.com>
2023-02-03 13:53:01 +0100

    fix: svg transformer inline

--- @ornikar/jest-config-react/transformers/svg-transformer-inline.js
+++ @ornikar/jest-config-react/transformers/svg-transformer-inline.js
@@ -4,14 +4,16 @@ const path = require('path');
 
 exports.process = function process(src, filePath) {
   const assetFilename = JSON.stringify(path.basename(filePath));
-  return `
-const { jsx } = require('react/jsx-runtime');
-function JestSvgComponent(props) {
-return jsx(
-  'svg',
-  Object.assign({}, props, {'data-file-name': ${assetFilename}})
-);
-}
-module.exports = JestSvgComponent;
-          `;
+  return {
+    code: `
+    const { jsx } = require('react/jsx-runtime');
+    function JestSvgComponent(props) {
+    return jsx(
+      'svg',
+      Object.assign({}, props, {'data-file-name': ${assetFilename}})
+    );
+    }
+    module.exports = JestSvgComponent;
+              `,
+  };
 };
