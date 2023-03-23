3d7230cc82e1c4bce2b0a30eea662fba11987f73
Christophe Hurpeau <christophe@hurpeau.com>
2023-03-23 10:58:35 +0100

    fix: update transformers for jest 29

--- @ornikar/jest-config-react/transformers/svg-transformer.js
+++ @ornikar/jest-config-react/transformers/svg-transformer.js
@@ -5,22 +5,24 @@ const { process } = require('./svg-transformer-inline');
 
 exports.process = (src, filePath) => {
   const assetFilename = JSON.stringify(path.basename(filePath));
-  const content = process(src, filePath);
-  return content.replace(
-    /module.exports = (.*);/,
-    `module.exports = new Proxy({}, {
-    get: function getter(target, key) {
-      if (key === '__esModule') {
-        return true;
-      }
-      if (key === 'default') {
-        return ${assetFilename};
-      }
-      if (key === 'ReactComponent') {
-        return $1;
-      }
-      throw new Error('Invalid key for svg-transformer jest mock: ' + key);
+  const { code } = process(src, filePath);
+  return {
+    code: code.replace(
+      /module.exports = (.*);/,
+      `module.exports = new Proxy({}, {
+  get: function getter(target, key) {
+    if (key === '__esModule') {
+      return true;
     }
-  });`,
-  );
+    if (key === 'default') {
+      return ${assetFilename};
+    }
+    if (key === 'ReactComponent') {
+      return $1;
+    }
+    throw new Error('Invalid key for svg-transformer jest mock: ' + key);
+  }
+});`,
+    ),
+  };
 };
