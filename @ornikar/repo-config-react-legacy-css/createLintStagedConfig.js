3dacc47b418b71e8dd966a7d3cb14c4cff7e0d42
Christophe Hurpeau <christophe@hurpeau.com>
2023-04-20 14:22:14 +0200

    feat(repo-config-react)!: create repo-config-react-legacy-css with typed-css-modules [no issue] (#883)
    BREAKING CHANGE: if you used css files, replace repo-config-react by repo-config-react-legacy-css

---
+++ @ornikar/repo-config-react-legacy-css/createLintStagedConfig.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const createBaseLintStagedConfig = require('@ornikar/repo-config-react/createLintStagedConfig');
+
+const pkg = JSON.parse(fs.readFileSync(path.resolve('package.json')));
+
+module.exports = function createLintStagedConfig(options = {}) {
+  const config = createBaseLintStagedConfig(options);
+
+  const srcDirectories = createBaseLintStagedConfig.getSrcDirectories(options.srcDirectoryName);
+
+  if (pkg.devDependencies.typescript) {
+    Object.assign(config, {
+      [`${srcDirectories}/**/*.module.{css,css.d.ts}`]: (filenames) => ["tcm -s -p '**/*.module.css'"],
+    });
+  }
+
+  return config;
+};
