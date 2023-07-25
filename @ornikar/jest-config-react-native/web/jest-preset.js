d505b2c7ab6e971778fed3825b051568fdafb911
Christophe Hurpeau <christophe@hurpeau.com>
2023-07-25 10:30:33 +0200

    feat(jest-config-react): configure export custom conditions for web

--- @ornikar/jest-config-react-native/web/jest-preset.js
+++ @ornikar/jest-config-react-native/web/jest-preset.js
@@ -9,6 +9,7 @@ module.exports = {
   ...baseOrnikarPreset,
   ...expoPreset,
   testEnvironment: baseOrnikarPreset.testEnvironment, // override testEnvironment in expo preset
+  testEnvironmentOptions: baseOrnikarPreset.testEnvironmentOptions,
   snapshotResolver: require.resolve('../snapshot-resolvers/resolver.web.js'),
   setupFiles: [...expoPreset.setupFiles, ...baseOrnikarPreset.setupFiles, require.resolve('../test-setup')],
   testMatch: [
