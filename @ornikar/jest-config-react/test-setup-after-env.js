20bc17407ed4a00da05bedd10f093b08b3b23093
theohdv <theodevilleneuve@gmail.com>
2023-08-01 16:03:45 +0200

    refactor: remove duplicate check

--- @ornikar/jest-config-react/test-setup-after-env.js
+++ @ornikar/jest-config-react/test-setup-after-env.js
@@ -18,15 +18,6 @@ failOnConsole({
       return true;
     }
 
-    // This was making App.test.tsx fail for an unknown reason because we don't use componentWillMount at all in our codebase
-    if (
-      message.includes(
-        'componentWillMount has been renamed, and is not recommended for use. See https://reactjs.org/link/unsafe-component-lifecycles for detail',
-      )
-    ) {
-      return true;
-    }
-
     // Do not ignore the act / await warning anymore once we are on React 18
     if (message.includes('You called act(async () => ...) without await')) return true;
 
