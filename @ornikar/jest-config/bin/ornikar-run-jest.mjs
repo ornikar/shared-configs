df4134f2eb5c7ea7ca683716f435da09933c90ea
Christophe Hurpeau <christophe@hurpeau.com>
2023-10-25 17:43:24 +0200

    feat(jest-config): add ornikar-run-jest

---
+++ @ornikar/jest-config/bin/ornikar-run-jest.mjs
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+import { spawnSync } from 'node:child_process';
+import { lt } from 'semver';
+
+const nodeVersion = process.versions.node;
+
+if (lt(nodeVersion, '21.1.0')) {
+  console.warn(
+    `Use node version >= 21.1.0 to fix issue with jest. Actual: ${nodeVersion}. For more information, see https://github.com/jestjs/jest/issues/11956`,
+  );
+  spawnSync(
+    process.execPath,
+    [...process.execArgv, '--no-compilation-cache', 'node_modules/jest/bin/jest.js', ...process.argv.slice(2)],
+    {
+      stdio: 'inherit',
+    },
+  );
+} else {
+  if (process.env.NODE_ENV == null) {
+    process.env.NODE_ENV = 'test';
+  }
+  import('jest').then((module) => module.default.run());
+}
