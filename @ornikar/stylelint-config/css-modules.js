34b8479762991615cd34bdc569c1f0d98b3916d9
Christophe Hurpeau <christophe@hurpeau.com>
2023-11-08 18:07:31 +0100

    feat!: update stylelint

--- @ornikar/stylelint-config/css-modules.js
+++ @ornikar/stylelint-config/css-modules.js
@@ -3,15 +3,16 @@
 const sharedRules = require('./rules/shared-rules');
 
 module.exports = {
-  extends: ['stylelint-config-standard', 'stylelint-config-css-modules', 'stylelint-config-prettier'].map(
-    require.resolve,
-  ),
+  extends: ['stylelint-config-standard', 'stylelint-config-css-modules'].map(require.resolve),
 
   plugins: ['stylelint-order'].map(require.resolve),
 
   rules: {
     ...sharedRules,
     'no-descending-specificity': null,
+    'selector-class-pattern': null,
+    'media-feature-range-notation': null,
+    'declaration-block-no-redundant-longhand-properties': null,
 
     // override default rule to disable for composes, used in css modules
     // https://stylelint.io/user-guide/rules/value-keyword-case
