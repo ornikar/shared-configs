a9e5a8e53a930ae12dce89138afd2833a121da34
Christophe Hurpeau <christophe@hurpeau.com>
2023-11-22 16:45:31 +0100

    fix(commitlint-config): remove max length not in specification [no issue] (#1039)

--- @ornikar/commitlint-config/index.js
+++ @ornikar/commitlint-config/index.js
@@ -6,5 +6,9 @@ module.exports = {
     // disable scope case
     // We use camelCase in directory names, and scope should match these directories in some cases.
     'scope-case': [0],
+
+    // not in the spec, see https://www.conventionalcommits.org/en/v1.0.0/#specification
+    'header-max-length': [0],
+    'footer-max-line-length': [0],
   },
 };
