e81162e01205d5515c9fbea6a03aa275a04b8cb9
Romain Spielmann <romain.spielmann@ornikar.com>
2023-11-29 14:37:20 +0100

    fix: ornikar package custom transform

--- @ornikar/jest-config-react-native/customTransforms.js
+++ @ornikar/jest-config-react-native/customTransforms.js
@@ -6,7 +6,9 @@
 
 exports.customTransforms = {
   // compilation of ornikar packages
-  'node_modules/@ornikar/.*\\.(js|cjs|mjs)$': require.resolve('./transformers/babel-transformer-ornikar-packages.js'),
+  'node_modules/@ornikar/(.[a-z-]*)/dist/.*\\.(js|cjs|mjs)$': require.resolve(
+    './transformers/babel-transformer-ornikar-packages.js',
+  ),
 
   // compilation of problematic node_modules has a simpler babel config
   'node_modules/(react-native-(calendars|reanimated)|@react-native-community/netinfo|@react-native/virtualized-lists)/.*\\.(js|jsx|ts|tsx)$':
