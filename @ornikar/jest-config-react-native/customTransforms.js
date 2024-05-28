b1f5caef05abf3c6d5aa78e166f9f855344c4bba
Theo de Villeneuve <theodevilleneuve@gmail.com>
2024-05-28 12:32:05 +0200

    feat: trying new jest config

--- @ornikar/jest-config-react-native/customTransforms.js
+++ @ornikar/jest-config-react-native/customTransforms.js
@@ -10,30 +10,11 @@ exports.customTransforms = {
     './transformers/babel-transformer-ornikar-packages.js',
   ),
 
-  // compilation of problematic node_modules has a simpler babel config
-  'node_modules/(react-native-(calendars|reanimated)|@react-native-community/netinfo|@react-native/virtualized-lists)/.*\\.(js|jsx|ts|tsx)$':
-    require.resolve('./transformers/babel-transformer-node-modules.js'),
-
   // dont transform node_modules when already compiled
   'node_modules/.*/(commonjs|dist/(cjs|vendor)|build/)/.*\\.js$': require.resolve(
     './transformers/identity-transformer.js',
   ),
 
-  // compilation of react-native with sucrase fails since expo 48 update
-  'node_modules/react-native/.*\\.(js|jsx|ts|tsx)$': require.resolve(
-    './transformers/babel-transformer-node-modules.js',
-  ),
-
-  // https://github.com/expo/expo/blob/d5d454eb585bdd8fb1a07e2910ba99dca8fd8786/packages/@expo/metro-config/src/transformer/createMultiRuleTransformer.ts#L207
-  'node_modules/(expo-processing|@expo/vector-icons)/.*\\.(js|jsx|ts|tsx)$': [
-    '@sucrase/jest-plugin',
-    { transforms: ['jsx', 'imports'] },
-  ],
-  'node_modules/(expo-assets-utils)/.*\\.(js|jsx|ts|tsx)$': [
-    '@sucrase/jest-plugin',
-    { transforms: ['flow', 'imports'] },
-  ],
-
   // compilation of rest node_modules with sucrase
-  'node_modules.*\\.(js|jsx|ts|tsx)$': '@sucrase/jest-plugin',
+  'node_modules.*\\.(js|jsx|ts|tsx)$': require.resolve('./transformers/babel-transformer-node-modules.js'),
 };
