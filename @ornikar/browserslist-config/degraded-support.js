95844e84555068b4401451bb772dfa3943ba1b4c
colinerenuy <coline.renuy@ornikar.com>
2025-05-06 12:56:39 +0200

    fix: prettier

--- @ornikar/browserslist-config/degraded-support.js
+++ @ornikar/browserslist-config/degraded-support.js
@@ -2,4 +2,5 @@
 
 // Note: if you update this, update the link in README.md !
 
-module.exports = 'last 4 versions, > .2% in FR, Firefox ESR, not ie < 10, not ios < 10, not op_mini all, not dead'.split(',');
+module.exports =
+  'last 4 versions, > .2% in FR, Firefox ESR, not ie < 10, not ios < 10, not op_mini all, not dead'.split(',');
