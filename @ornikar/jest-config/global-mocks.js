f114b95d3e7850c2995cad07636f81b7594c211e
Greg OLIVIER <gregoire.olivier@ornikar.com>
2025-06-26 16:35:13 +0200

    feat: remove mockdate fropm global mocks

--- @ornikar/jest-config/global-mocks.js
+++ @ornikar/jest-config/global-mocks.js
@@ -1,8 +1,8 @@
 'use strict';
 
-const mockdate = require('mockdate');
+// const mockdate = require('mockdate');
 
-mockdate.set('2018-07-30T08:52:42.679Z');
+// mockdate.set('2018-07-30T08:52:42.679Z');
 
 // Mock getFullYear
 Date.getFullYear = () => 2019;
