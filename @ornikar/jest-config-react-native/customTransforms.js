e35dc360588b09ad67e7fccca8d42c2c9fb27922
Paul Sniezak <paul.sniezak@ornikar.com>
2025-11-13 15:28:54 +0100

    feat(jest-config-react-native): remove sucrase OSE-19637 (#1134)
    Co-authored-by: colinerenuy <coline.renuy@ornikar.com>

--- @ornikar/jest-config-react-native/customTransforms.js
+++ @ornikar/jest-config-react-native/customTransforms.js
@@ -10,26 +10,15 @@ exports.customTransforms = {
     './transformers/babel-transformer-ornikar-packages.js',
   ),
 
-  // compilation of problematic node_modules has a simpler babel config
-  'node_modules/(react-native-(calendars|reanimated)|@react-native-community/netinfo|@react-native/virtualized-lists)/.*\\.(js|jsx|ts|tsx)$':
+  // The default Expo babel preset is not applied as we have our own babel config
+  // See https://github.com/expo/expo/blob/sdk-52/packages/jest-expo/src/resolveBabelConfig.js
+  'node_modules/(expo|expo-.*|@expo.*|react-native|react-native-(calendars|reanimated)|@react-native-community/netinfo|@react-native/virtualized-lists)/.*\\.(js|jsx|ts|tsx)$':
     require.resolve('./transformers/babel-transformer-node-modules.js'),
 
   // dont transform node_modules when already compiled
   'node_modules/.*/(commonjs|dist/(cjs|vendor)|build/)/.*\\.js$': require.resolve(
     './transformers/identity-transformer.js',
   ),
-
-  // compilation of react-native with sucrase fails since expo 48 update
-  // compilation of expo packages with sucrase fails since expo 52 update
-  'node_modules/(expo|expo-.*|@expo.*|react-native)/.*\\.(js|jsx|ts|tsx)$': require.resolve(
-    './transformers/babel-transformer-node-modules.js',
-  ),
-
-  // https://github.com/expo/expo/blob/d5d454eb585bdd8fb1a07e2910ba99dca8fd8786/packages/@expo/metro-config/src/transformer/createMultiRuleTransformer.ts#L207
-  'node_modules/(expo-processing|@expo/vector-icons|expo-assets-utils)/.*\\.(js|jsx|ts|tsx)$': [
-    '@sucrase/jest-plugin',
-    { transforms: ['jsx', 'imports'] },
-  ],
 
   // compilation of rest node_modules with sucrase
   'node_modules.*\\.(js|jsx|ts|tsx)$': '@sucrase/jest-plugin',
