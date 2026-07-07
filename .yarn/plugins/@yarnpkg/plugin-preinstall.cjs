d74f24bd811239c59137af5a74635263a66e6147
leorniduv <leopold.duverger@ornikar.com>
2026-07-07 11:51:24 +0200

    feat: add missing yarn config

---
+++ .yarn/plugins/@yarnpkg/plugin-preinstall.cjs
@@ -0,0 +1,30 @@
+module.exports = {
+  name: `plugin-preinstall`,
+  factory: (require) => {
+    const { execSync } = require('child_process');
+    const { existsSync, readFileSync } = require('fs');
+    const path = require('path');
+
+    return {
+      hooks: {
+        validateProject: async (project) => {
+          const pkgJsonPath = path.join(project.cwd, 'package.json');
+          if (existsSync(pkgJsonPath)) {
+            const pkgJson = JSON.parse(readFileSync(pkgJsonPath, 'utf8'));
+            if (pkgJson.scripts && pkgJson.scripts.preinstall) {
+              try {
+                execSync(pkgJson.scripts.preinstall, {
+                  cwd: project.cwd,
+                  stdio: 'inherit',
+                  shell: true,
+                });
+              } catch (e) {
+                // preinstall failed
+              }
+            }
+          }
+        },
+      },
+    };
+  },
+};
